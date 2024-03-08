2e5ef0118294df7a1b3365c79df114324e62c3a7
SneiderCub <sneidercubides@outlook.com>
2024-03-07 22:15:42 -0500

    finally

--- mean_var_std.py
+++ mean_var_std.py
@@ -7,8 +7,16 @@ def calculate(list):
         input = np.array(list).reshape(3,3)
         mean = [input.mean(axis=0).tolist(),input.mean(axis=1).tolist(),input.mean().tolist()]
         variance = [input.var(axis=0).tolist(),input.var(axis=1).tolist(),input.var().tolist()]
+        standard_deviation = [input.std(axis=0).tolist(),input.std(axis=1).tolist(),input.std().tolist()]
+        max = [input.max(axis=0).tolist(),input.max(axis=1).tolist(),input.max().tolist()]
+        min = [input.min(axis=0).tolist(),input.min(axis=1).tolist(),input.min().tolist()]
+        sum = [input.sum(axis=0).tolist(),input.sum(axis=1).tolist(),input.sum().tolist()]
         calculations = {
             'mean': mean,
-            'variance': variance
-            }
+            'variance': variance,
+            'standard deviation': standard_deviation,
+            'max':max,
+            'min':min,
+            'sum':sum
+}
     return calculations
